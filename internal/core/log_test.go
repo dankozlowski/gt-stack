513955cf22549cdf0ae6e741b265fb07cc198797
Dan Kozlowski <dakozlowski@gmail.com>
2026-05-07 16:27:06 -0700

    formatting

--- internal/core/log_test.go
+++ internal/core/log_test.go
@@ -11,14 +11,14 @@ import (
 func TestLogTree_RendersStack(t *testing.T) {
 	fr := &git.FakeRunner{
 		Responses: map[string]git.FakeResponse{
-			"symbolic-ref --short HEAD":                         {Stdout: []byte("feat/b\n")},
-			"config --get gts.trunk":                            {Stdout: []byte("main\n")},
+			"symbolic-ref --short HEAD":                          {Stdout: []byte("feat/b\n")},
+			"config --get gts.trunk":                             {Stdout: []byte("main\n")},
 			"for-each-ref --format=%(refname:short) refs/heads/": {Stdout: []byte("main\nfeat/a\nfeat/b\n")},
-			"config --get branch.main.gts-parent":               {ExitCode: 1},
-			"config --get branch.feat/a.gts-parent":             {Stdout: []byte("main\n")},
-			"config --get branch.feat/b.gts-parent":             {Stdout: []byte("feat/a\n")},
-			"config --get branch.feat/a.gts-pr":                 {ExitCode: 1},
-			"config --get branch.feat/b.gts-pr":                 {ExitCode: 1},
+			"config --get branch.main.gts-parent":                {ExitCode: 1},
+			"config --get branch.feat/a.gts-parent":              {Stdout: []byte("main\n")},
+			"config --get branch.feat/b.gts-parent":              {Stdout: []byte("feat/a\n")},
+			"config --get branch.feat/a.gts-pr":                  {ExitCode: 1},
+			"config --get branch.feat/b.gts-pr":                  {ExitCode: 1},
 		},
 	}
 	c := New(git.New(fr), nil)
